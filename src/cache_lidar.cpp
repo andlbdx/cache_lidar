310b32f2ad613748e14085656e4c5ecde8c7b18a
andlbdx <andlbds@163.com>
2025-01-14 14:48:29 +0800

    新增订阅和发布

--- src/cache_lidar.cpp
+++ src/cache_lidar.cpp
@@ -9,6 +9,10 @@ cache_lidar::cache_lidar() {
     nh.param<int>("/cache_lidar/frame_num", frame_num, 5);
     nh.param<std::string>("/cache_lidar/sub_topic", sub_topic, "/livox/lidar");
     nh.param<std::string>("/cache_lidar/pub_topic", pub_topic, "/cache/lidar");
+
+
+    sub_cloud_ = nh.subscribe(sub_topic, 1, &cache_lidar::cloudCallback, this);
+    pub_cloud_ = nh.advertise<sensor_msgs::PointCloud2>(pub_topic, 10);
 }
 
 void cache_lidar::cloudCallback(const sensor_msgs::PointCloud2::ConstPtr &cloud_msg) {
